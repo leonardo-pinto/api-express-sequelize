033e8b677583300109c888dbc8e3bd19887cd57c
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-01 10:46:33 -0300

    feat(app): add initial express server

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,14 @@
-const test = 'faisal';
-console.log(`My name is xxx${test}`);
+import Database from './database'
+import dbConfig from './config/database'
+import env from './config/environment'
+
+(async () => {
+  try {
+    const db = new Database(env.nodeEnv, dbConfig)
+    await db.connect()
+    const app = (await import('./app')).default
+    app.listen(env.port, () => console.log(`Server running at port ${env.port}`))
+  } catch (error) {
+    console.log(`Something went wrong when initializing the server: ${error.message}`)
+  }
+})()
