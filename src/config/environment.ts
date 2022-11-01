adffff619a64e6def0aa92298f8e02edabbeafbc
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-01 09:04:55 -0300

    feat(sequelize): add database env variables file

---
+++ src/config/environment.ts
@@ -0,0 +1,6 @@
+export default {
+  port: parseInt(process.env.PORT) || 8080,
+  nodeEnv: process.env.NODE_ENV || 'production',
+  saltRounds: parseInt(process.env.SALT_ROUNDS) || 12,
+  jwtSecret: process.env.JWT_SECRET || 'd02c8340e40905f2a2e3e94cdsjk95a54b02e51de79b93938bceea65e1d7280e'
+}
