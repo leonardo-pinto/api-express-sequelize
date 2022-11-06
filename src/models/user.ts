442cf7e6d6184ed08fb377dd82b0578754493c57
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-06 08:19:08 -0300

    feat(user): add hash and compared password methods to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,6 @@
-import { Table, Model, Column, DataType, AllowNull, Unique } from 'sequelize-typescript'
+import { Table, Model, Column, DataType, AllowNull, Unique, BeforeSave, AfterSave } from 'sequelize-typescript'
+import bcrypt from 'bcrypt'
+import env from '../config/environment'
 
 @Table({
   timestamps: true,
@@ -10,11 +12,6 @@ export default class User extends Model {
   @Unique(true)
   @Column(DataType.STRING)
   email!: string
-
-  @AllowNull(false)
-  @Unique(true)
-  @Column(DataType.STRING)
-  username!: string
 
   @AllowNull(false)
   @Column(DataType.STRING)
@@ -31,4 +28,16 @@ export default class User extends Model {
   @AllowNull(false)
   @Column(DataType.STRING)
   role!: string
+
+  @BeforeSave
+  static async hashPassword(user: User) {
+    if (user.password) {
+      const hashedPassword =  await bcrypt.hash(user.password, env.saltRounds)
+      user.password = hashedPassword
+    }
+  }
+
+  static async comparePasswords(password: string, hashedPassword: string) {
+    return await bcrypt.compare(password, hashedPassword)
+  }
 }
