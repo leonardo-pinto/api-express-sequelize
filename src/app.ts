3b1c8623660b8354eaf72af5935be294f9774db0
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-06 08:14:10 -0300

    feat(app): add errorsMiddleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import setupMiddlewares from './config/middlewares'
 import setupRoutes from './config/routes'
+import errorsMiddleware from './middlewares/errors'
 
 const app = express()
 setupMiddlewares(app)
 setupRoutes(app)
+app.use(errorsMiddleware)
 export default app
