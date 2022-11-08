ffdd4dd3982684b30a81b97180bc8c7469581e39
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-08 09:56:32 -0300

    test(jwt-utils): add verifyAccessToken method unit tests

--- tests/utils/jwt-utils.test.ts
+++ tests/utils/jwt-utils.test.ts
@@ -1,8 +1,25 @@
-import JwtUtils from '../../src/utils/jwt-utils'
+import jwt from 'jsonwebtoken'
+import { JwtUtils } from '../../src/utils/jwt-utils'
 
 describe('Jwt Utils', () => {
-  test('Should return an access token', () => {
-    const payload = 'any_payload'
-    expect(JwtUtils.generateAccessToken(payload)).toEqual(expect.any(String))
+
+  describe('generateAccessToken()', () => {
+    test('Should return an access token', () => {
+      const payload = { id: 1, role: 'any' }
+      expect(JwtUtils.generateAccessToken(payload)).toEqual(expect.any(String))
+    })
+  })
+
+  describe('verifyAccessToken()', () => {
+    test('Should verify that the access token is valid', () => {
+      const payload = { id: 1, role: 'any' }
+      const jwt = JwtUtils.generateAccessToken(payload)
+      const result = JwtUtils.verifyAccessToken(jwt)
+      expect(result).toEqual(expect.objectContaining(payload))
+    })
+
+    test('Should throw error if access token is invalid', () => {
+      expect(() => JwtUtils.verifyAccessToken('invalid.token')).toThrow(jwt.JsonWebTokenError)
+    })
   })
 })
