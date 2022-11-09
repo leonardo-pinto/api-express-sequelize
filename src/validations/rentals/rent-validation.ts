63260bc95088699ff80c250069f17c6db6314458
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-09 17:52:26 -0300

    refactor(rent-validation): change bookId type to string

--- src/validations/rentals/rent-validation.ts
+++ src/validations/rentals/rent-validation.ts
@@ -1,9 +1,9 @@
-import { IsNotEmpty, IsNumber } from "class-validator"
+import { IsNotEmpty, IsString } from "class-validator"
 
 export class RentValidation {
 
   @IsNotEmpty()
-  @IsNumber()
+  @IsString()
   bookId!: string
 
 }
