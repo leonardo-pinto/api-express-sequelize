285f742e12c59d5b00f36605461ed83f694cb6ce
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-09 09:24:39 -0300

    test(login-controller): remove unused console.log and change request-validator assertions

--- tests/controllers/login-controller.test.ts
+++ tests/controllers/login-controller.test.ts
@@ -8,18 +8,15 @@ describe('Login Controller', () => {
   let app
 
   beforeAll(async () => {
-    console.log('beforeAll')
     await TestsHelper.startDb()
     app = await TestsHelper.getApp()
   })
 
   afterAll(async () => {
-    console.log('afterAll')
     await TestsHelper.stopDb()
   })
 
   beforeEach(async () => {
-    console.log('beforeEach')
     await TestsHelper.syncDb()
   })
 
@@ -29,11 +26,14 @@ describe('Login Controller', () => {
       const response = await request(app).post('/api/v1/register')
       .send({
         email: 'test@example.com',
+        firstName: 'any_firstName',
+        lastName: 'any_lastName',
         role: 'admin'
       }).expect(400)
 
       expect(response.body.error.message).toEqual('Request validation failed')
-      expect(response.body.error.errors).toEqual(expect.any(Array))
+      expect(response.body.error.errors[0]).toEqual('password must be a string')
+      expect(response.body.error.errors[1]).toEqual('password should not be empty')
       expect(response.body.error.stack).toEqual(expect.any(String))
     })
 
