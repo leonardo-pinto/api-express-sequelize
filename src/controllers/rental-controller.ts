f7825c3ad58745095eb82880414e3aef88a808fe
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-09 11:04:20 -0300

    feat(rental): add rental controller

---
+++ src/controllers/rental-controller.ts
@@ -0,0 +1,76 @@
+import { Request } from 'express'
+import Rental from '../models/rental'
+import { SuccessResponse } from "../utils/success-response"
+import Book from '../models/book'
+import { DateTime } from 'luxon'
+import { BadRequestError } from '../utils/api-error'
+
+export default class RentalController {
+
+  async rent(req: Request) {
+    const { bookId } = req.body
+    const { userId } = req
+    const rentalDate = DateTime.now()
+    const dueDate = DateTime.now().plus({ days: 7 })
+
+    await Rental.create({
+      bookId,
+      userId,
+      rentalDate,
+      dueDate
+    })
+
+    return new SuccessResponse(201)
+  }
+
+  async renew(req: Request) {
+    const { rentalId } = req.params
+
+    const rental = await Rental.findOne({
+      where: { id: rentalId }
+    })
+
+    if (DateTime.fromJSDate(rental.dueDate) > DateTime.now()) {
+      await Rental.update(
+        { dueDate: DateTime.fromJSDate(rental.dueDate).plus({ days: 7 }) },
+        { where: { id: rentalId } }
+      )
+    } else {
+      return new BadRequestError('The given rental is overdue. It is not possible to renew it')
+    }
+
+    return new SuccessResponse(200)
+  }
+
+  async getById(req: Request) {
+    const { rentalId } = req.params
+    const rental = await Rental.findOne({
+      where: { id: rentalId },
+      include: Book
+    })
+
+    return new SuccessResponse(200, rental)
+  }
+
+  async getByUserId(req: Request) {
+    const { userId } = req
+
+    const rentals = await Rental.findAll({
+      where: { userId },
+      include: Book
+    })
+
+    return new SuccessResponse(200, { rentals })
+  }
+
+  async getByBookId(req: Request) {
+    const { bookId } = req.params
+
+    const rentals = await Rental.findAll({
+      where: { bookId },
+      include: Book
+    })
+
+    return new SuccessResponse(200, { rentals })
+  }
+}
