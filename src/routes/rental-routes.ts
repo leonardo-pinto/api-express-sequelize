90d9bf85e99fbf6b0001526cf4756e1ea5f7351f
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-09 11:04:53 -0300

    feat(rental): add rental routes

---
+++ src/routes/rental-routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import RentalController from '../controllers/rental-controller';
+import { Auth, RequestValidator } from '../middlewares';
+import { asyncWrapper } from '../utils/async-wrapper';
+import { RentValidation } from '../validations/rentals/rent-validation';
+
+const rentalController = new RentalController()
+
+export default (router: Router): void => {
+  router.post(
+    '/rental',
+    Auth.handle(['admin', 'user']),
+    RequestValidator.validate(RentValidation),
+    asyncWrapper(rentalController.rent)
+  ),
+  router.put(
+    '/rental/:rentalId/renew',
+    Auth.handle(['admin', 'user']),
+    asyncWrapper(rentalController.renew)
+  ),
+  router.get(
+    '/rental/:rentalId',
+    Auth.handle(['admin', 'user']),
+    asyncWrapper(rentalController.getById)
+  ),
+  router.get(
+    '/rental/user',
+    Auth.handle(['admin', 'user']),
+    asyncWrapper(rentalController.getByUserId)
+  ),
+  router.get(
+    '/rental/book/:bookId',
+    Auth.handle(['admin', 'user']),
+    asyncWrapper(rentalController.getByBookId)
+  )
+}
