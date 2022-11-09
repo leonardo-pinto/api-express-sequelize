e700eba0f34e40c5be411f799cabd96a30e5989b
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-09 09:21:12 -0300

    test(tests-helper): add createNewUser and createNewBook methods

--- tests/tests-helper.ts
+++ tests/tests-helper.ts
@@ -1,5 +1,6 @@
 import Database from '../src/database'
 import dbConfig from '../src/config/database'
+import request from 'supertest'
 
 let db: Database
 
@@ -24,4 +25,29 @@ export default class TestsHelper {
     return app
   }
 
+  static async createNewUser(role?: string) {
+    const app = await TestsHelper.getApp()
+    return request(app)
+      .post('/api/v1/register')
+      .send({
+        email: 'test@example.com',
+        password: 'any_password',
+        firstName: 'Leonardo',
+        lastName: 'Pinto',
+        role: role ?? 'admin'
+      })
+  }
+
+  static async createNewBook(accessToken: string) {
+    const app = await TestsHelper.getApp()
+    return request(app)
+      .post('/api/v1/book')
+      .set('authorization', accessToken)
+      .send({
+        title: 'any_title',
+        subject: 'any_subject',
+        author: 'any_author',
+        publisher: 'any_publisher'
+      })
+  }
 }
