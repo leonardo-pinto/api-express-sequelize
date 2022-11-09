87d207e818be15aeb310fe6dc3fc08231aff94a9
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-09 11:05:22 -0300

    feat(rental): add rental model

---
+++ src/models/rental.ts
@@ -0,0 +1,32 @@
+import { Table, Model, Column, DataType, AllowNull, BelongsTo, ForeignKey } from 'sequelize-typescript'
+import Book from './book'
+import User from './user'
+
+@Table({
+  timestamps: true,
+  tableName: 'Rentals'
+})
+export default class Rental extends Model {
+
+  @ForeignKey(() => Book)
+  @Column(DataType.INTEGER)
+  bookId!: number
+
+  @ForeignKey(() => User)
+  @Column(DataType.INTEGER)
+  userId!: number
+
+  @AllowNull(false)
+  @Column(DataType.DATE)
+  rentalDate!: string
+
+  @AllowNull(false)
+  @Column(DataType.DATE)
+  dueDate!: Date
+
+  @BelongsTo(() => Book)
+  book: Book
+
+  @BelongsTo(() => User)
+  user: User
+}
