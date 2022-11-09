01a317691d931653ce44154e32436460812b6227
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-09 17:53:31 -0300

    feat(test-helpers): add users, books, and rentals helper methods

--- tests/tests-helper.ts
+++ tests/tests-helper.ts
@@ -1,6 +1,8 @@
 import Database from '../src/database'
 import dbConfig from '../src/config/database'
 import request from 'supertest'
+import User from '../src/models/user'
+import Book from '../src/models/book'
 
 let db: Database
 
@@ -26,28 +28,95 @@ export default class TestsHelper {
   }
 
   static async createNewUser(role?: string) {
+    return User.create({
+      email: 'valid_email@example.com',
+      password: 'valid_password',
+      firstName: 'John',
+      lastName: 'Doe',
+      role: role ?? 'admin'
+    })
+  }
+
+  static async createNewUserRequest(role?: string) {
     const app = await TestsHelper.getApp()
     return request(app)
       .post('/api/v1/register')
       .send({
-        email: 'test@example.com',
-        password: 'any_password',
+        email: 'valid_email@example.com',
+        password: 'valid_password',
         firstName: 'Leonardo',
         lastName: 'Pinto',
         role: role ?? 'admin'
       })
   }
 
-  static async createNewBook(accessToken: string) {
+  static async loginRequest(email?: string, password?: string) {
+    const app = await TestsHelper.getApp()
+    return request(app)
+      .post('/api/v1/login')
+      .send({
+        email: email ?? 'valid_email@example.com',
+        password: password ?? 'valid_password'
+      })
+  }
+
+  static async createNewBookRequest(accessToken: string) {
     const app = await TestsHelper.getApp()
     return request(app)
       .post('/api/v1/book')
-      .set('authorization', accessToken)
+      .set('authorization', `Bearer ${accessToken}`)
       .send({
         title: 'any_title',
         subject: 'any_subject',
         author: 'any_author',
         publisher: 'any_publisher'
       })
+  }
+
+  static async createNewBook() {
+    return Book.create({
+      title: 'any_title',
+      subject: 'any_subject',
+      author: 'any_author',
+      publisher: 'any_publisher'
+    })
+  }
+
+  static async createNewRentRequest(accessToken: string) {
+    const app = await TestsHelper.getApp()
+    return request(app)
+      .post('/api/v1/rental')
+      .set('authorization', `Bearer ${accessToken}`)
+      .send({
+        bookId: '1'
+      })
+  }
+
+  static async renewRequest(accessToken: string, id?: string) {
+    const rentalId = id ?? '1'
+    const app = await TestsHelper.getApp()
+    return request(app)
+      .put(`/api/v1/rental/${rentalId}/renew`)
+      .set('authorization', `Bearer ${accessToken}`)
+  }
+
+  static async deleteBookRequest(bookId: string, accessToken: string) {
+    const app = await TestsHelper.getApp()
+    return request(app).delete(`/api/v1/book/${bookId}`)
+      .set('authorization', `Bearer ${accessToken}`)
+  }
+
+  static async getAllBooksRequest(accessToken: string) {
+    const app = await TestsHelper.getApp()
+    return request(app).get('/api/v1/book')
+    .set('authorization', `Bearer ${accessToken}`)
+    .expect(200)
+  }
+
+  static async returnRentalRequest(accessToken: string) {
+    const app = await TestsHelper.getApp()
+    return request(app)
+      .put(`/api/v1/rental/1/return`)
+      .set('authorization', `Bearer ${accessToken}`)
   }
 }
