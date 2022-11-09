36c7829914fc18f6638dce5efd46c7adfd449543
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-11-09 08:57:21 -0300

    fix(login-routes): fix loginController instance name

--- src/routes/login-routes.ts
+++ src/routes/login-routes.ts
@@ -1,14 +1,13 @@
 import { Router } from 'express'
 import LoginController from '../controllers/login-controller'
-import RequestValidator from '../middlewares/request-validator'
+import { RequestValidator } from '../middlewares'
 import { asyncWrapper } from '../utils/async-wrapper'
-import { LoginValidation } from '../validations/login-validation'
-import { RegisterValidation } from '../validations/register-validation'
+import { LoginValidation } from '../validations/login/login-validation'
+import { RegisterValidation } from '../validations/login/register-validation'
 
-
-const authController = new LoginController()
+const loginController = new LoginController()
 
 export default (router: Router): void => {
-  router.post('/register', RequestValidator.validate(RegisterValidation),asyncWrapper(authController.register))
-  router.post('/login', RequestValidator.validate(LoginValidation),asyncWrapper(authController.login))
+  router.post('/register', RequestValidator.validate(RegisterValidation), asyncWrapper(loginController.register))
+  router.post('/login', RequestValidator.validate(LoginValidation), asyncWrapper(loginController.login))
 }
